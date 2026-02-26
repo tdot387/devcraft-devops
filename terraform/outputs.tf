caf59184394a470c45207c7662c68b99abb989c9
Thomas Wagner <t-wagner@web.de>
2026-02-26 13:15:57 +0100

    removed terraform data

--- terraform/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "server_ip" {
-  description = "Öffentliche IP-Adresse des Servers"
-  value       = hcloud_server.app.ipv4_address
-}
-
-output "ssh_command" {
-  description = "SSH-Befehl zum Verbinden"
-  value       = "ssh root@${hcloud_server.app.ipv4_address}"
-}
