a0317b01843039c813452beefdf6a6c6ce29f554
Dinu, Andrei Cristian <e01631854@student.tuwien.ac.at>
2026-03-06 12:53:25 +0100

    Add prisma client instance

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const notesRouter = require("./routes/notes");
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
 
 const app = express();
 
